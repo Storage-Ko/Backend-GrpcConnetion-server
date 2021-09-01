f66907610de48a5f73b7ab0603844c60b8b0be02
KoGyeongtae <kokt0203@naver.com>
2021-09-01 22:56:45 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Grpc Study server
+
+## Grpc를 이용한 MSA 구축을 공부하기 위해 만든 서버 레포지토리입니다.
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/Backend-Grpc-server/data"
+	userpb "github.com/Backend-Grpc-server/protos/user"
+)
+
+const portNumber = "9000"
+
+type userServer struct {
+	userpb.UserServer
+}
+
+// GetUser returns user message by user_id
+func (s *userServer) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
+	userID := req.UserId
+
+	var userMessage *userpb.UserMessage
+	for _, u := range data.UserData {
+		if u.UserId != userID {
+			continue
+		}
+		userMessage = u
+		break
+	}
+
+	return &userpb.GetUserResponse{
+		UserMessage: userMessage,
+	}, nil
+}
+
+// ListUsers returns all user messages
+func (s *userServer) ListUsers(ctx context.Context, req *userpb.ListUsersRequest) (*userpb.ListUsersResponse, error) {
+	userMessages := make([]*userpb.UserMessage, len(data.UserData))
+	for i, u := range data.UserData {
+		userMessages[i] = u
+	}
+
+	return &userpb.ListUsersResponse{
+		UserMessages: userMessages,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":"+portNumber)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	userpb.RegisterUserServer(grpcServer, &userServer{})
+
+	log.Printf("start gRPC server on %s port", portNumber)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
